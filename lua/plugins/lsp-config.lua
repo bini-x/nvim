4c55e9b938abe450f2ea21904d6cee8bf1773119
bini-x <erblinhiseni@gmail.com>
2024-06-22 17:43:39 +0200

    new

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,51 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tsserver" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.emmet_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+
+			local isLspDiagnosticsVisible = true
+			vim.keymap.set("n", "<leader>lx", function()
+				isLspDiagnosticsVisible = not isLspDiagnosticsVisible
+				vim.diagnostic.config({
+					virtual_text = isLspDiagnosticsVisible,
+					underline = isLspDiagnosticsVisible,
+				})
+			end)
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
