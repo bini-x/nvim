4f7ae07a87fb3f8f045a9a211ce7bab3449578f4
bini-x <erblinhiseni@gmail.com>
2024-06-24 15:20:18 +0200

    added neotest

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,24 @@
+return {
+	"nvim-neotest/neotest",
+	"nvim-neotest/neotest-jest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-jest")({
+					jestCommand = "npm test --",
+					jestConfigFile = "custom.jest.config.ts",
+					env = { CI = true },
+					cwd = function(path)
+						return vim.fn.getcwd()
+					end,
+				}),
+			},
+		})
+	end,
+}
