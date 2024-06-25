691ef54965dff00f5a12eb67bc1c8a6eb0a09119
bini-x <erblinhiseni@gmail.com>
2024-06-25 20:42:13 +0200

    new

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,20 +5,79 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("catppuccin-mocha")
 			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				background = { -- :h background
+					light = "latte",
+					dark = "mocha",
+				},
+				transparent_background = false,
+				show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+				term_colors = false,
+				dim_inactive = {
+					enabled = false,
+					shade = "dark",
+					percentage = 0.15,
+				},
+				no_italic = true, -- Force no italic
+				no_bold = false, -- Force no bold
+				styles = {
+					comments = { "italic" },
+					conditionals = { "italic" },
+					loops = {},
+					functions = {},
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+					operators = {},
+				},
+				color_overrides = {},
+				custom_highlights = {},
 				integrations = {
 					cmp = true,
 					gitsigns = true,
 					nvimtree = true,
-					treesitter = true,
+					telescope = true,
 					notify = false,
-					mini = {
-						enabled = true,
-						indentscope_color = "",
-					},
+					mini = false,
+					treesitter = true,
+					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+				},
+				highlight_overrides = {
+					all = function(colors)
+						return {
+							NvimTreeNormal = { fg = colors.none },
+							CmpBorder = { fg = "#3e4145" },
+						}
+					end,
+					latte = function(latte)
+						return {
+							Normal = { fg = latte.base },
+						}
+					end,
+					frappe = function(frappe)
+						return {
+							["@comment"] = { fg = frappe.surface2, style = { "italic" } },
+						}
+					end,
+					macchiato = function(macchiato)
+						return {
+							LineNr = { fg = macchiato.overlay1 },
+						}
+					end,
+					mocha = function(mocha)
+						return {
+							Comment = {},
+						}
+					end,
 				},
 			})
+			-- setup must be called before loading
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,37 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = { "lua", "javascript" },
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = {
+					enable = true,
+					disable = {},
+					additional_vim_regex_highlighting = false,
+				},
+				indent = { enable = true, disable = {} },
+				ensure_installed = {
+					"bash",
+					"c",
+					"cpp",
+					"css",
+					"dockerfile",
+					"go",
+					"html",
+					"javascript",
+					"json",
+					"lua",
+					"python",
+					"rust",
+					"scss",
+					"typescript",
+					"tsx",
+					"yaml",
+				},
+				autotag = { enable = true },
+			})
+		end,
+	},
 }
