e801856294d9c32ddecc8ef0ff19672019e3d6ac
bini-x <erblinhiseni@gmail.com>
2024-09-26 17:16:00 +0200

    new

--- lua/plugins/nordic.lua
+++ lua/plugins/nordic.lua
@@ -1,5 +1,5 @@
 return {
-  'AlexvZyl/nordic.nvim',
+  'AlexvZyl/nordic.nvim/',
   lazy = false,
   priority = 1000,
   config = function()
@@ -7,9 +7,10 @@ return {
     vim.api.nvim_set_hl(0, 'LineNr', { fg = '#bbc3d4' })
     vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#60728a' })
     vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#60728a' })
-    vim.api.nvim_set_hl(0, 'NeoTreeGitUntracked', { fg = '#c5727a' })
-    vim.api.nvim_set_hl(0, 'NeoTreeRootName', { fg = '#81a1c1' })
-    vim.api.nvim_set_hl(0, 'NeoTreeTitleBar', { fg = '#d79784' })
+    vim.api.nvim_set_hl(0, 'NeoTreeGitUntracked', { fg = '#d08770' })
+    vim.api.nvim_set_hl(0, 'NeoTreeRootName', { fg = '#e5e9f0' })
+    vim.api.nvim_set_hl(0, 'NeoTreeTitleBar', { fg = '#3b4252', bg = '#eceff4'})
+    vim.api.nvim_set_hl(0, 'NeoTreeFloatBorder', { fg = '#eceff4'})
   end,
 
 }
