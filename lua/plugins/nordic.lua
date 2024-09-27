ff5caa718692d70a17d0323a946cf8851d2f1d8d
bini-x <erblinhiseni@gmail.com>
2024-09-27 09:21:15 +0200

    new

--- lua/plugins/nordic.lua
+++ lua/plugins/nordic.lua
@@ -10,7 +10,7 @@ return {
     vim.api.nvim_set_hl(0, 'NeoTreeGitUntracked', { fg = '#d08770' })
     vim.api.nvim_set_hl(0, 'NeoTreeRootName', { fg = '#e5e9f0' })
     vim.api.nvim_set_hl(0, 'NeoTreeTitleBar', { fg = '#3b4252', bg = '#eceff4'})
-    vim.api.nvim_set_hl(0, 'NeoTreeFloatBorder', { fg = '#eceff4'})
+    vim.api.nvim_set_hl(0, 'NeoTreeFloatBorder', { fg = '#eceff4', bg="#242933"})
   end,
 
 }
