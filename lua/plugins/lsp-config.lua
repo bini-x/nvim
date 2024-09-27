cb55ef2c58e6bd74cb98967b9b8c7dd0d0d58da5
bini-x <erblinhiseni@gmail.com>
2024-09-27 10:21:04 +0200

    new

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,9 +18,9 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      -- lspconfig.lua_ls.setup({
-      --   capabilities = capabilities,
-      -- })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
       lspconfig.tsserver.setup({
         capabilities = capabilities,
       })
