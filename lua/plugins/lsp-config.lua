0e784fc1bd3ddba605f72dc97b454608f07ec75f
bini-x <erblinhiseni@gmail.com>
2025-04-09 23:52:33 +0200

    fixed lualine, added some tools for csharp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,65 +1,70 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "html", "emmet_ls", "gopls" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      -- lspconfig.lua_ls.setup({
-      --   capabilities = capabilities,
-      -- })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.emmet_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-        filetypes = { "go", "gomod", "gowork", "gotmpl" },
-        settings = {
-          gopls = {
-            completeUnimported = true,
-            usePlaceholders = true,
-            analyses = {
-              unusedparams = true
-            }
-          }
-        }
-      })
-      lspconfig.jedi_language_server.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls", "html", "emmet_ls", "gopls", "omnisharp" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.omnisharp.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+				cmd = { "dotnet", "/home/bini/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.emmet_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				settings = {
+					gopls = {
+						completeUnimported = true,
+						usePlaceholders = true,
+						analyses = {
+							unusedparams = true,
+						},
+					},
+				},
+			})
+			lspconfig.jedi_language_server.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
 
-      local isLspDiagnosticsVisible = true
-      vim.keymap.set("n", "<leader>lx", function()
-        isLspDiagnosticsVisible = not isLspDiagnosticsVisible
-        vim.diagnostic.config({
-          virtual_text = isLspDiagnosticsVisible,
-          underline = isLspDiagnosticsVisible,
-        })
-      end)
+			local isLspDiagnosticsVisible = true
+			vim.keymap.set("n", "<leader>lx", function()
+				isLspDiagnosticsVisible = not isLspDiagnosticsVisible
+				vim.diagnostic.config({
+					virtual_text = isLspDiagnosticsVisible,
+					underline = isLspDiagnosticsVisible,
+				})
+			end)
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,75 @@
-return { -- Bubbles config for lualine
-	-- Author: lokesh-krishna
-	-- MIT license, see LICENSE for more details.
+return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		local colors = {
+			blue = "#539bf5",
+			cyan = "#76E3EA",
+			black = "#22272e",
+			white = "adbac7",
+			red = "#A05350",
+			violet = "#DCBDFB",
+			grey = "#303030",
+			green = "#8DDB8C",
+		}
+
+		local bubbles_theme = {
+			normal = {
+				a = { fg = colors.black, bg = colors.blue },
+				b = { fg = colors.white, bg = colors.grey },
+				c = { fg = colors.white, bg = colors.black },
+			},
+
+			insert = { a = { fg = colors.black, bg = colors.green } },
+			visual = { a = { fg = colors.black, bg = colors.cyan } },
+			replace = { a = { fg = colors.black, bg = colors.red } },
+			command = { a = { fg = colors.black, bg = colors.violet } },
+
+			inactive = {
+				a = { fg = colors.white, bg = colors.black },
+				b = { fg = colors.white, bg = colors.black },
+				c = { fg = colors.white, bg = colors.black },
+			},
+		}
+
+		vim.api.nvim_create_autocmd({ "UiEnter", "ColorScheme" }, {
+			callback = function()
+				local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+				local statusline = vim.api.nvim_get_hl(0, { name = "StatusLine" })
+				-- Create a new table for the updated highlight group
+				local updated_statusline = vim.tbl_extend("force", statusline, { bg = normal.bg })
+				vim.api.nvim_set_hl(0, "StatusLine", updated_statusline)
+			end,
+		})
+
 		require("lualine").setup({
-			options = { theme = "github_dark_dimmed" },
+			options = {
+				theme = bubbles_theme,
+				component_separators = "",
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+				lualine_b = { "filename", "branch" },
+				lualine_c = {
+					"%=", --[[ add your center components here in place of this comment ]]
+				},
+				lualine_x = {},
+				lualine_y = { "filetype", "progress" },
+				lualine_z = {
+					{ "location", separator = { right = "" }, left_padding = 2 },
+				},
+			},
+			inactive_sections = {
+				lualine_a = { "filename" },
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = { "location" },
+			},
+			tabline = {},
+			extensions = {},
 		})
 	end,
 }
