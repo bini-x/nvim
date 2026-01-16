febac7fd6382f87ed30456297607ead576dfcf5a
bini-x <erblinhiseni@gmail.com>
2026-01-16 19:56:23 +0100

    added diffview

--- lua/plugins/githubtheme.lua
+++ lua/plugins/githubtheme.lua
@@ -1,12 +1,12 @@
 return {
-	"projekt0n/github-nvim-theme",
-	name = "github-theme",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		require("github-theme").setup({
-			vim.api.nvim_set_hl(0, "OilDir", { fg = "#539bf5" }),
-		})
-		vim.cmd("colorscheme github_dark_dimmed")
-	end,
+  "projekt0n/github-nvim-theme",
+  name = "github-theme",
+  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require("github-theme").setup({
+      vim.api.nvim_set_hl(0, "OilDir", { fg = "#539bf5" }),
+    })
+    vim.cmd("colorscheme github_dark_dimmed")
+  end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,8 +2,7 @@ return {
   {
     "williamboman/mason.nvim",
     config = function()
-      require("mason").setup({
-      })
+      require("mason").setup({})
     end,
   },
   {
@@ -12,72 +11,123 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
-          "ts_ls",
+          "ts_ls", -- Using ts_ls
           "html",
           "emmet_ls",
           "gopls",
           "jdtls",
           "eslint",
-          "omnisharp",
+          "clangd",
         },
       })
     end,
   },
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
+
+      -- Common on_attach function for all LSP servers
+      local on_attach = function(client, bufnr)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr })
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
+      end
+
+      -- Use the new vim.lsp.start API for each server
+      -- Lua
+      vim.lsp.start({
+        name = "lua_ls",
+        cmd = { "lua-language-server" },
         capabilities = capabilities,
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-        filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-      })
-      lspconfig.omnisharp.setup({
-        capabilities = capabilities,
-        cmd = { "dotnet", "/home/bini/.local/share/nvim/mason/packages/omnisharp/OmniSharp.dll" },
-        filetypes = { "cs" },
-      })
-      lspconfig.eslint.setup({
-        filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+        on_attach = on_attach,
         settings = {
-          packageManager = "npm",
-        },
+          Lua = {
+            runtime = { version = 'LuaJIT' },
+            diagnostics = { globals = { 'vim' } },
+            workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+            telemetry = { enable = false },
+          }
+        }
+      })
+
+      -- TypeScript/JavaScript - USING TS_LS
+      vim.lsp.start({
+        name = "ts_ls",
+        cmd = { "typescript-language-server", "--stdio" },
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+      })
+
+      -- HTML
+      vim.lsp.start({
+        name = "html",
+        cmd = { "vscode-html-language-server", "--stdio" },
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { "html" }
+      })
+
+      -- Emmet
+      vim.lsp.start({
+        name = "emmet_ls",
+        cmd = { "emmet-ls", "--stdio" },
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      -- Go
+      vim.lsp.start({
+        name = "gopls",
+        cmd = { "gopls" },
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { "go", "gomod", "gowork", "gotmpl" },
+      })
+
+      -- Java
+      vim.lsp.start({
+        name = "jdtls",
+        cmd = { "jdtls" },
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      -- ESLint
+      vim.lsp.start({
+        name = "eslint",
+        cmd = { "vscode-eslint-language-server", "--stdio" },
+        capabilities = capabilities,
         on_attach = function(client, bufnr)
+          on_attach(client, bufnr)
           vim.api.nvim_create_autocmd("BufWritePre", {
             buffer = bufnr,
             command = "EslintFixAll",
           })
         end,
-      })
-      lspconfig.jdtls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.emmet_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-        filetypes = { "go", "gomod", "gowork", "gotmpl" },
-        settings = {
-          gopls = {
-            completeUnimported = true,
-            usePlaceholders = true,
-            analyses = {
-              unusedparams = true,
-            },
-          },
-        },
-      })
-      lspconfig.jedi_language_server.setup({
-        capabilities = capabilities,
+        filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
       })
 
+      -- C/C++
+      vim.lsp.start({
+        name = "clangd",
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=never",
+          "--completion-style=detailed",
+        },
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      -- Your existing diagnostics toggle
       local isLspDiagnosticsVisible = true
       vim.keymap.set("n", "<leader>lx", function()
         isLspDiagnosticsVisible = not isLspDiagnosticsVisible
@@ -86,10 +136,6 @@ return {
           underline = isLspDiagnosticsVisible,
         })
       end)
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
 }
